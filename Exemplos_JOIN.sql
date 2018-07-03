9e9e3863b4dfb6047a3578204af6d93163ffb284
LuisFelipeDeSousa <luisfelipecontatosystem@hotmail.com>
2018-07-03 16:24:17 -0300

    princesaLeia

---
+++ Exemplos_JOIN.sql
@@ -0,0 +1,48 @@
+﻿-- Venda de carros --
+
+DROP TABLE carros; -- Para apagar determinados dados
+DROP TABLE pessoas;
+
+-- Criação das tabelas
+
+CREATE TABLE Pessoas(Id INT IDENTITY, 
+Nome			VARCHAR(200), 
+Cpf				VARCHAR(15));
+
+ALTER TABLE Pessoas ADD CONSTRAINT PKPesoas PRIMARY KEY(Id);
+
+CREATE TABLE Carros(Id INT IDENTITY, 
+IdPessoa			INT, 
+Marca				VARCHAR(150) NOT NULL, 
+Modelo				VARCHAR(150) NOT NULL, 
+AnoLancamento		INT NOT NULL, 
+AnoFabricacao		INT NOT NULL,
+Motor				VARCHAR(30),
+Cor					VARCHAR(100),
+Preco				DECIMAL(9,2));
+
+ALTER TABLE Carros ADD CONSTRAINT PKCarros PRIMARY KEY(Id);
+ALTER TABLE Carros ADD CONSTRAINT FKPessoas FOREIGN KEY(IdPessoa) REFERENCES Pessoas(Id);
+
+INSERT INTO Pessoas VALUES
+('Abraão Nobre', '952.328.294-83'),
+('Severino Braga', '876.770.333-00'),
+('Samuel Faria', '989.272.034-29'),
+('Florêncio Robalo', '362.635.174-25');
+
+INSERT INTO Carros(IdPessoa, Marca, Modelo, AnoLancamento, AnoFabricacao, Motor, Cor, Preco)VALUES
+((SELECT Id FROM Pessoas WHERE Nome = 'Abraão Nobre'), 'Volkswagen', 'Gol', 2010, 2009, '8v Power Flex', 'Vermelho', 18000.00);
+
+INSERT INTO Carros(IdPessoa, Marca, Modelo, AnoLancamento, AnoFabricacao, Motor, Cor, Preco)VALUES
+((SELECT Id FROM Pessoas WHERE Nome = 'Severino Braga'), 'Fiat', 'Bravo', 2000, 1999, 'SX 1.6 16V', 'Cinza', 9000.00);
+
+INSERT INTO Carros(IdPessoa, Marca, Modelo, AnoLancamento, AnoFabricacao, Motor, Cor, Preco)VALUES
+((SELECT Id FROM Pessoas WHERE Nome = 'Samuel Faria'), 'Renault', 'Clio', 1997, 1996, '1.0 8v', 'Verde', 5500.00);
+
+INSERT INTO Carros(IdPessoa, Marca, Modelo, AnoLancamento, AnoFabricacao, Motor, Cor, Preco)VALUES
+((SELECT Id FROM Pessoas WHERE Nome = 'Florêncio Robalo'), 'Volkswagen', 'Golf', 1994, 1994, '2.0 120cv', 'Azul', 17000.00);
+
+SELECT * FROM Carros;
+SELECT * FROM Pessoas;
+
+
---
+++ Exercicio_01.sql
@@ -0,0 +1,53 @@
+﻿SELECT * FROM habilidades;
+SELECT * FROM herois;
+
+SELECT 
+	herois.nome 'Personagem',
+	habilidades 'Habilidade'
+FROM habilidades
+JOIN herois ON (habilidades.id_heroi = herois.id);
+
+--OBJETIVO DO 'JOIN' > Trazer informação de outra tabela.
+
+-- JOIN PARA PEGAR POKEMONS DE CATEGORIA
+SELECT 
+name 'Nome', categories.name 'Categoria' 
+FROM pokemons
+JOIN categorias ON (categories.id = pokemons.id_categoryI)
+WHERE name = 'Bulbasaur'
+
+-- JOIN PARA PEGAR CATEGORIA
+
+SELECT categories.name 'Categoria', pokemons.name 'Pokemon'
+FROM categories
+JOIN pokemons ON(categories.id = pokemons.categoryI)
+ORDER BY categories.name, pokemons.name; -- ORDENAR CATEGORIAS
+
+-- JOIN PARA PEGAR QUANTIDADE DE POKEMONS 'BUG'
+
+SELECT COUNT (pokemons.id) 'Quantidade de Pokemons BUG'
+FROM categories
+JOIN pokemons ON(pokemons.id_categoryI = categories.id)
+WHERE categories.name = 'Bug';
+
+-- GROUP BY --> SERVE PARA ARMAZENAR A QUANTIDADE DE MASSA DE ESTATISTICA 
+
+SELECT tipo, SUM (valor)
+FROM -- DA TABELA X
+/*GROUP BY*/ tipo 
+
+SELECT COUNT (pokemons.id) 'Quantidade de Pokemons BUG'
+FROM categories
+JOIN pokemons ON(pokemons.id_categoryI = categories.id)
+WHERE categories.name = 'Bug';
+
+SELECT categories.name, COUNT (pokemons.id) 'Quantidade de Pokemons'
+FROM categories
+JOIN pokemons ON(pokemons.id_categorYI = categories.id)
+GROUP BY  categories.name
+ORDER BY COUNT (pokemons.id)DESC;
+
+-- GROUP BY -- Serve para pegar informações e agrupar informações
+
+
+
